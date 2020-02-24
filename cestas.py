8a20d4ee2e3083938609d5290a5368ad65a6a916
José Gabriel González <jgg032899@gmail.com>
2020-02-23 22:37:35 -0400

    Comentarios y modificaciones

--- cestas.py
+++ cestas.py
@@ -1,27 +1,32 @@
 import itertools
 
-div = {}
-sup = {}
-filt_sup = {}
+div = {}  # Divisores (item sets)
+sup = {}  # Soporte para cada item set de 1 elemento
+filt_sup = {}  # Soporte filtado para cada item set de 1 elemento
+two_sup = {}  # Soporte para cada item set de 2 elementos
+
 
 def generate_numbers():
-  for i in range(1,101):
-    div[i] = []
-    for j in range(1,101):
-      if i % j == 0:
-        div[i].append(j)
+    for i in range(1, 101):
+        div[i] = []
+        for j in range(1, 101):
+            if i % j == 0:
+                div[i].append(j)
+
 
 def get_sup():
-  for i in range(1,101):
-    sup[i] = 0
-    for j in range(1,101):
-      if i in div[j]:
-        sup[i] += 1
+    for i in range(1, 101):
+        sup[i] = 0
+        for j in range(1, 101):
+            if i in div[j]:
+                sup[i] += 1
+
 
 def filter_sup():
-  for i in range(1,101):
-    if sup[i] >= 5:
-      filt_sup[i] = sup[i]
+    for i in range(1, 101):
+        if sup[i] >= 5:
+            filt_sup[i] = sup[i]
+
 
 generate_numbers()
 
@@ -29,34 +34,27 @@ get_sup()
 
 filter_sup()
 
-combinations_two = list(itertools.combinations(list(filt_sup.keys()), 2))
+print('PRODUCTOS FRECUENTES: ')
+print(list(filt_sup.keys()))
 
-two_sup = {}
+combinations_two = list(itertools.combinations(
+    list(filt_sup.keys()), 2))  # Combinaciones de dos elementos
 
 for comb in combinations_two:
-  two_sup[comb] = 0
-  for d in div:
-    count = 0
-    for num in comb:
-      if num in div[d]:
-        count += 1
-    if count == 2:
-      two_sup[comb] += 1
-
-# print(two_sup)
+    two_sup[comb] = 0
+    for d in div:
+        count = 0
+        for num in comb:
+            if num in div[d]:
+                count += 1
+        if count == 2:
+            two_sup[comb] += 1
 
 filt_two_sup = {}
 
 for i in two_sup:
-  if two_sup[i] >= 5:
-    filt_two_sup[i] = two_sup[i]
+    if two_sup[i] >= 5:
+        filt_two_sup[i] = two_sup[i]
 
-print(filt_two_sup)
-
-filt_nums = []
-
-for i in filt_two_sup:
-  for j in i:
-    filt_nums.append(j)
-
-# print(list(set(filt_nums)))
+print('PARES DE PRODUCTOS FRECUENTES: ')
+print(list(filt_two_sup.keys()))
